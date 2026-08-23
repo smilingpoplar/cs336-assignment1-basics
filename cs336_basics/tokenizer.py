3d8c9f08328a0c10a29c99531febb2a63d0b0efc
le <smilingpoplar@gmail.com>
2026-08-23 16:15:27 +0800

    + test_tokenizer: multiprocessing encode

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,8 +1,68 @@
+import multiprocessing as mp
 import pickle
 from collections.abc import Iterable, Iterator
+from dataclasses import dataclass
 from math import inf
 
 import regex as re
+
+
+def bpe(
+    pt: tuple[bytes, ...],
+    merge_to_rank: dict[tuple[bytes, bytes], int],
+) -> tuple[bytes, ...]:
+    # pretoken内的合并
+    while True:
+        if len(pt) <= 1:
+            break
+        rank_idx_list = [(merge_to_rank.get((pt[i], pt[i + 1]), inf), i) for i in range(len(pt) - 1)]
+        best_rank, idx = min(rank_idx_list)
+        if best_rank == inf:
+            break
+        pt = pt[:idx] + (pt[idx] + pt[idx + 1],) + pt[idx + 2 :]
+    return pt
+
+
+def encode(
+    text: str,
+    vocab_inv: dict[bytes, int],
+    merge_to_rank: dict[tuple[bytes, bytes], int],
+    st_set: set[str],
+    st_pattern: str,
+) -> list[int]:
+    ids: list[int] = []
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    chunks = re.split(st_pattern, text) if st_set else [text]
+    for chunk in chunks:
+        if chunk in st_set:
+            ids.append(vocab_inv[chunk.encode("utf-8")])
+        else:
+            for match in re.finditer(PAT, chunk):
+                pt = match.group().encode("utf-8")
+                pt = tuple(pt[i : i + 1] for i in range(len(pt)))  # tuple[bytes, ...]
+                pt = bpe(pt, merge_to_rank)
+                ids += [vocab_inv[bs] for bs in pt]
+    return ids
+
+
+@dataclass(frozen=True)
+class WorkerState:
+    vocab_inv: dict[bytes, int]
+    merge_to_idx: dict[tuple[bytes, bytes], int]
+    st_set: set[str]
+    st_pattern: str
+
+
+_state: WorkerState
+
+
+def init_worker(state: WorkerState):
+    global _state
+    _state = state
+
+
+def encode_worker(text: str) -> list[int]:
+    return encode(text, _state.vocab_inv, _state.merge_to_idx, _state.st_set, _state.st_pattern)
 
 
 class Tokenizer:
@@ -31,36 +91,18 @@ class Tokenizer:
             merges = pickle.load(f)
         return cls(vocab, merges, special_tokens)
 
-    def _bpe(self, pt: tuple[bytes, ...]) -> tuple[bytes, ...]:
-        # pretoken内的合并
-        while True:
-            if len(pt) <= 1:
-                break
-            rank_idx_list = [(self._merge_to_rank.get((pt[i], pt[i + 1]), inf), i) for i in range(len(pt) - 1)]
-            best_rank, idx = min(rank_idx_list)
-            if best_rank == inf:
-                break
-            pt = pt[:idx] + (pt[idx] + pt[idx + 1],) + pt[idx + 2 :]
-        return pt
-
     def encode(self, text: str) -> list[int]:
-        ids: list[int] = []
-        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-        chunks = re.split(self._st_pattern, text) if self._st_set else [text]
-        for chunk in chunks:
-            if chunk in self._st_set:
-                ids.append(self._vocab_inv[chunk.encode("utf-8")])
-            else:
-                for match in re.finditer(PAT, chunk):
-                    pt = match.group().encode("utf-8")
-                    pt = tuple(pt[i : i + 1] for i in range(len(pt)))  # tuple[bytes, ...]
-                    pt = self._bpe(pt)
-                    ids += [self._vocab_inv[bs] for bs in pt]
-        return ids
+        return encode(text, self._vocab_inv, self._merge_to_rank, self._st_set, self._st_pattern)
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
-        for line in iterable:
-            yield from self.encode(line)
+        num_processes = max(1, mp.cpu_count() - 2)
+        with mp.Pool(
+            processes=num_processes,
+            initializer=init_worker,
+            initargs=(WorkerState(self._vocab_inv, self._merge_to_rank, self._st_set, self._st_pattern),),
+        ) as pool:
+            for ids in pool.imap(encode_worker, iterable, chunksize=2):
+                yield from ids
 
     def decode(self, ids: list[int]) -> str:
         return b"".join([self._vocab[id] for id in ids]).decode("utf-8", errors="replace")
