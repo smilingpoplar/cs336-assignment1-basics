eefc2b9a3d385fb20df70d9b0aa78a4238281266
le <smilingpoplar@gmail.com>
2026-08-26 11:00:57 +0800

    + test_softmax

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -4,3 +4,10 @@ from jaxtyping import Float
 
 def silu(x: Float[torch.Tensor, "..."]) -> Float[torch.Tensor, "..."]:
     return x * torch.sigmoid(x)
+
+
+def softmax(x: Float[torch.Tensor, "..."], dim: int) -> Float[torch.Tensor, "..."]:
+    m = x.amax(dim, keepdim=True)
+    x = (x - m).exp()
+    x = x / x.sum(dim, keepdim=True)
+    return x
