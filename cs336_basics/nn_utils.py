fcddcfb1e3c225014be3c8e51d75f0aa9bc9a43d
le <smilingpoplar@gmail.com>
2026-08-27 16:40:14 +0800

    + test_cross_entropy

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -1,6 +1,6 @@
 import einx
 import torch
-from jaxtyping import Bool, Float
+from jaxtyping import Bool, Float, Int
 
 
 def silu(x: Float[torch.Tensor, "..."]) -> Float[torch.Tensor, "..."]:
@@ -26,3 +26,16 @@ def scaled_dot_product_attention(
         qk.masked_fill_(~mask, -1e9)
     a = softmax(qk, dim=-1)
     return einx.dot("... seq_q [seq_k], ... [seq_k] d_v -> ... seq_q d_v", a, V)
+
+
+def cross_entropy(
+    inputs: Float[torch.Tensor, "batch_size vocab_size"],
+    targets: Int[torch.Tensor, " batch_size"],
+) -> Float[torch.Tensor, ""]:
+    # log(softmax(x))的写法，会让softmax(偏离最大x值较远的x项)下溢为0
+    # logsumexp写法：
+    batch_size = inputs.shape[-2]
+    target_logits = inputs[torch.arange(batch_size), targets]
+    m = inputs.amax(dim=-1, keepdim=True)
+    log_sum_exp = (inputs - m).exp().sum(dim=-1).log() + m
+    return (log_sum_exp - target_logits).mean()
